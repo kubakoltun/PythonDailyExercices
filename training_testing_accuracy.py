23ef154e5850b493e012b03d1cda3e15937439be
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-01-16 18:54:25 +0100

    Create training_testing_accuracy.py

---
+++ training_testing_accuracy.py
@@ -0,0 +1,19 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+
+x, y = make_moons(
+    n_samples=500,
+    random_state=42,
+    noise=0.3
+)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+
+
+knn = KNeighborsClassifier(n_neighbors=42)
+knn.fit(x_train, y_train)
+train_acc = knn.score(x_train, y_train)
+
+print("training accuracy: %f" % train_acc)
+test_acc = knn.score(x_test, y_test)
+print("testing accuracy: %f" % test_acc)
