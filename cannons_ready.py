479294698b72fd5416812ff4c5ccc9ccd345bcd0
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-02-16 17:21:32 +0100

    Create cannons_ready.py

---
+++ cannons_ready.py
@@ -0,0 +1,9 @@
+def cannons_ready(gunners):
+    response = ''
+    for i in gunners:
+        if (gunners[i] == 'nay'):
+           response = 'Shiver me timbers!'
+        elif (response != 'Shiver me timbers!'):
+            response = 'Fire!'
+            
+    return response
