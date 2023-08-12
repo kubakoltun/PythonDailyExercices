1531e72611a00cbab61a3c0f331a46022030ad4b
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-08-12 17:00:41 +0200

    Update alternating_case.py

--- alternating_case.py
+++ alternating_case.py
@@ -1,8 +1,10 @@
 def to_alternating_case(string):
     news = ''
+    
     for i in range (len(string)):
         if(string[i].islower()):
             news += string[i].upper()
         else:
             news += string[i].lower()
+            
     return news
